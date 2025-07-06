7d2b0b6857bf55216460b33771b3ff92297e16b0
devauradigital <devauradigital@gmail.com>
2025-07-06 18:28:49 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,33 +1,41 @@
 const express = require('express');
 const app = express();
 const server = require('http').createServer(app);
-const io = require('socket.io')(server);
+const io = require('socket.io')(server, {
+    cors: {
+        origin: '*', // Adjust to specific origins in production
+        methods: ['GET', 'POST']
+    }
+});
+const { v4: uuidv4 } = require('uuid');
 const PORT = process.env.PORT || 3000;
 
 const rooms = {};
 
 io.on('connection', (socket) => {
-    socket.on('create-room', (roomId) => {
+    socket.on('create-room', (callback) => {
+        const roomId = uuidv4();
         if (rooms[roomId]) {
-            socket.emit('room-error', 'Room already exists');
+            socket.emit('room-error', { message: 'Room already exists', roomId });
             return;
         }
         rooms[roomId] = { users: [socket.id] };
         socket.join(roomId);
+        callback(roomId);
     });
 
     socket.on('join-room', (roomId) => {
         if (!rooms[roomId]) {
-            socket.emit('room-error', 'Room does not exist');
+            socket.emit('room-error', { message: 'Room does not exist', roomId });
             return;
         }
         if (rooms[roomId].users.length >= 2) {
-            socket.emit('room-error', 'Room is full');
+            socket.emit('room-error', { message: 'Room is full', roomId });
             return;
         }
         rooms[roomId].users.push(socket.id);
         socket.join(roomId);
-        socket.to(roomId).emit('user-joined');
+        socket.to(roomId).emit('user-joined', { roomId });
     });
 
     socket.on('offer', ({ roomId, offer }) => {
@@ -35,11 +43,26 @@ io.on('connection', (socket) => {
     });
 
     socket.on('answer', ({ roomId, answer }) => {
-        socket.to(roomId).emit('answer', { answer });
+        socket.to(roomId).emit('answer', { roomId, answer });
     });
 
     socket.on('ice-candidate', ({ roomId, candidate }) => {
-        socket.to(roomId).emit('ice-candidate', { candidate });
+        socket.to(roomId).emit('ice-candidate', { roomId, candidate });
+    });
+
+    socket.on('leave-room', (roomId) => {
+        if (rooms[roomId]) {
+            const index = rooms[roomId].users.indexOf(socket.id);
+            if (index !== -1) {
+                rooms[roomId].users.splice(index, 1);
+                if (rooms[roomId].users.length === 0) {
+                    socket.to(roomId).emit('room-deleted', { roomId });
+                    delete rooms[roomId];
+                } else {
+                    socket.to(roomId).emit('user-disconnected', { roomId });
+                }
+            }
+        }
     });
 
     socket.on('disconnect', () => {
@@ -49,9 +72,10 @@ io.on('connection', (socket) => {
             if (index !== -1) {
                 room.users.splice(index, 1);
                 if (room.users.length === 0) {
+                    socket.to(roomId).emit('room-deleted', { roomId });
                     delete rooms[roomId];
                 } else {
-                    socket.to(roomId).emit('user-disconnected');
+                    socket.to(roomId).emit('user-disconnected', { roomId });
                 }
             }
         }
